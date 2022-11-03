d178f42c5af41d524fa748ae95cf3aa1de7402d2
Brooke Heck <b19heck@gmail.com>
2022-11-03 17:31:39 +0000

    add error handling

--- src/models/tasks/tasks.js
+++ src/models/tasks/tasks.js
@@ -5,6 +5,7 @@ const taskModel = (sequelize, DataTypes) => sequelize.define('Tasks', {
   text: { type: DataTypes.STRING, required: true },
   assignee: {type: DataTypes.STRING, require: true },
   difficulty: {type: DataTypes.NUMBER, required: true },
+  complete: {type: DataTypes.BOOLEAN, required: true},
 });
 
 module.exports = taskModel;
